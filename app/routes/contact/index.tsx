226f2380e2d0bde9d4f8d83712e3db91a5a287c5
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-06-06 14:58:11 +0700

    chore: Update global styles for contact form
    ```

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -22,23 +22,14 @@ export default function Page() {
       </Link>
       <h2>Contact</h2>
       <form>
-        <fieldset
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignContent: "center",
-          }}
-        >
+        <fieldset>
           <legend>Get in Touch</legend>
           <label htmlFor="name">Name</label>
           <input type="text" id="name" name="name" required />
-
           <label htmlFor="email">Email</label>
           <input type="email" id="email" name="email" required />
-
           <label htmlFor="message">Message</label>
           <textarea id="message" name="message" required></textarea>
-
           <button type="submit">Submit</button>
         </fieldset>
       </form>
