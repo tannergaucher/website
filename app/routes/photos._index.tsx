8fb85c2c56d50dbcb2b60bf5343b1b106dc4cbbe
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-06-27 14:03:55 +0700

    Add title

--- app/routes/photos._index.tsx
+++ app/routes/photos._index.tsx
@@ -1,9 +1,13 @@
 import { LoaderFunction, LinksFunction } from "@remix-run/node";
-import { useLoaderData, Link } from "@remix-run/react";
+import { useLoaderData, Link, MetaFunction } from "@remix-run/react";
 
 import { getPhotoCollections } from "~/models/photo-collection.server";
 import globalStyles from "~/styles/global.css?url";
 import pageStyles from "~/styles/photos-page.css?url";
+
+export const meta: MetaFunction = () => {
+  return [{ title: "Tanner Gaucher | Photos" }];
+};
 
 export const links: LinksFunction = () => {
   return [
